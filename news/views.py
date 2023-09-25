45824b9f4ce98dc5461d4267af3c75f4610c9947
gus-aguiar <gustavopma90@gmail.com>
2023-09-25 13:59:27 -0300

    req4

--- news/views.py
+++ news/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import News
 
-# Create your views here.
+
+def home(request):
+    news_list = News.objects.all()
+    context = {"news_list": news_list}
+    return render(request, "home.html", context)
