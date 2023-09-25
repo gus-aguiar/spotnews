32381da3fec7499624b0c3c3c17b65dccac69c64
gus-aguiar <gustavopma90@gmail.com>
2023-09-25 18:35:53 -0300

    req6

--- news/urls.py
+++ news/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home-page"),
     path("news/<int:pk>", views.news_details, name="news-details-page"),
+    path("categories", views.categories_form, name="categories-form"),
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import News
+from .forms import CategoriesForm
 
 
 def home(request):
@@ -12,3 +13,18 @@ def news_details(request, pk):
     news_details = News.objects.get(pk=pk)
     context = {"news_details": news_details}
     return render(request, "news_details.html", context)
+
+
+def categories_form(request):
+    form = CategoriesForm()
+
+    if request.method == "POST":
+        form = CategoriesForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("home-page")
+
+    context = {"form": form}
+
+    return render(request, "categories_form.html", context)
